f59ee716eb9a72b128439f7647273afa41dde623
douglas86 <douglasmaxton@gmail.com>
2021-05-11 11:49:52 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { keys } from "./keys";
+
+const app = express();
+
+const CONNECTION_URL = keys.mongodb.connection;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: PORT`))
+  )
+  .catch((err) => err.message);
+
+mongoose.set("useFindAndModify", false);
