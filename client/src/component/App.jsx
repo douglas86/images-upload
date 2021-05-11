07848faeaa92154f8747517723d33f87067a649d
douglas86 <douglasmaxton@gmail.com>
2021-05-11 14:17:21 +0200

    Reset the client component

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import FileUpload from "./FileUpload.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
-      <FileUpload />
     </div>
   );
 };
