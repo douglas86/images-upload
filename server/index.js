f72f738558c82fcf98eb27ebaa47672356875d60
douglas86 <douglasmaxton@gmail.com>
2021-05-11 12:14:22 +0200

    Added a new path on express for uploading

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import fileUpload from "express-fileupload";
 import { keys } from "./config/keys";
 
 const app = express();
+
+app.use(fileUpload());
 
 const CONNECTION_URL = keys.mongodb.connection;
 const PORT = process.env.PORT || 5000;
@@ -18,3 +21,20 @@ mongoose
   .catch((err) => err.message);
 
 mongoose.set("useFindAndModify", false);
+
+// uplaod endpoint
+app.post(`/uplaod`, (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ mgs: "No file uploaded" });
+  }
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
